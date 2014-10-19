19c25d9bab0309ce804dbedafa4a5836610bbc26
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-10-19 17:06:27 +0200

    Implement prototype

---
+++ README.rst
@@ -0,0 +1,117 @@
+shackle
+=========
+
+.. image:: https://raw.github.com/wasamasa/shackle/master/img/shackle.gif
+
+About
+-----
+
+``shackle`` gives you the means to put an end to popped up buffers not
+behaving they way you'd like them to.  By setting up simple rules you
+can for instance make Emacs always select help buffers for you or make
+everything reuse your currently selected window.
+
+Installation
+------------
+
+Install via `quelpa <https://github.com/quelpa/quelpa>`_ with ``M-:
+(quelpa '(shackle :fetcher github :repo "wasamasa/shackle"))`` for the
+time being or download ``shackle.el``, place it into a suitable
+location such as ``~/.emacs.d/vendor/`` and add the following to your
+init file:
+
+.. code:: cl
+
+    (add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/"))
+
+Usage
+--------------
+
+First you need to customize ``shackle-rules``, this can be done via
+``M-x customize-group RET shackle`` or in your init file.
+
+As the name of the variable suggests, it's a list of rules.  Each rule
+consists of a condition and a set of key-value combinations that tell
+what to do with the buffer in question.
+
+The condition can be either a symbol, a string or ``t``.  A symbol is
+interpreted as the major mode of the buffer to match, a string as the
+name of the buffer (which can be turned into regexp matching by using
+the ``:regexp`` key with a value of ``t`` in the key-value part) and
+``t`` as the default rule to follow when no other match succeeds.  If
+you set up a default rule, make sure it's the last rule in
+``shackle-rules``, otherwise it will always be used.
+
+The following key-value pairs are available:
+
+- ``:select`` and ``t``:
+
+  Select the popped up window.
+
+- ``reuse`` and ``t``:
+
+  Reuse the current window.
+
+To have an exception to a default rule, use the condition of your
+choice and either don't list the key-value pair, use a different value
+or use a placeholder key with any value.
+
+The following example configuration enables the rather radical
+behaviour of always reusing the current window in order to avoid
+unwanted window splitting:
+
+.. code:: cl
+
+    (setq shackle-rules
+          '((t :reuse t)))
+
+This one on the other hand provides a less intrusive user experience
+and demonstrates how to use exceptions:
+
+.. code:: cl
+
+    (setq shackle-rules
+          '((compilation-mode :noselect t)
+            (t :select t)))
+
+Once you're done customizing ``shackle-rules``, use ``M-x
+shackle-mode`` to enable ``shackle`` interactively.  To enable it
+automatically on startup, add ``(shackle-mode -1)`` to your init file.
+
+Internals
+---------
+
+``shackle`` adds an extra entry to ``display-buffer-alist``, a
+customizable variable in Emacs that specifies what to do with buffers
+displayed with the ``display-buffer`` function.  It's used by quite a
+lot of Emacs packages, including very essential ones like the built-in
+help and compilation package.
+
+This means other Emacs packages that don't use this function won't be
+influenced by ``shackle``.  If you should ever come across a package
+that ought to use it, but doesn't conform, chances are you'll have to
+speak with upstream instead of me to have it fixed.  Another thing to
+be aware of is that if you've set up a default rule, it will take over
+the Emacs defaults which can play less well with packages (such as
+`Magit <http://github.com/magit/magit>`_).  Once you find out what's
+causing the problem, you can add an exception rule to fix it.
+
+Contributing
+------------
+
+If you find bugs, have suggestions or any other problems, feel free to
+report an issue on the issue tracker or hit me up on IRC, I'm always on
+``#emacs``.  Patches are welcome, too, just fork, work on a separate
+branch and open a pull request with it.
+
+Alternatives
+------------
+
+This package is heavily inspired by `popwin
+<https://github.com/m2ym/popwin-el>`_ and was hacked together after
+discovering it being hard to debug, creating overly many idle timers
+and exposing rather baffling bugs.  ``shackle`` being intentionally
+simpler and easier to understand is considered a debugging-friendly
+feature, not a bug.  However if you prefer less rough edges, a
+sensible default configuration and having more options for
+customizing, give ``popwin`` a try.
---
+++ shackle.el
@@ -0,0 +1,169 @@
+;;; shackle.el --- Enforce rules for popups
+
+;; Copyright (C) 2014 Vasilij Schneidermann <v.schneidermann@gmail.com>
+
+;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
+;; URL: https://github.com/wasamasa/shackle
+;; Version: 0.0.1
+;; Keywords: convenience
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING. If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; This global minor mode allows you to easily set up rules for
+;; popups in Emacs.
+
+;; See the README for more info:
+;; https://github.com/wasamasa/shackle
+
+;;; Code:
+
+(require 'cl-lib)
+
+(defgroup shackle nil
+  "Bla"
+  :group 'convenience
+  :prefix "shackle-")
+
+(defcustom shackle-lighter " ⛓"
+  "Lighter for `shackle-mode'"
+  :type 'string
+  :group 'shackle)
+
+(defcustom shackle-rules nil
+  "Association list of rules what to do with windows.
+Each rule consists of a condition and a property list.  The
+condition can be a symbol, a string or t.  If it's a symbol,
+match the buffer's major mode.  If it's a string, match the name
+of the buffer.  Use the following option in the property list to
+use regular expression matching:
+
+:regexp t
+
+If the condition is t, turn the property list into the default
+for every invocation of `display-buffer'.
+
+The property list accepts the following keys and values:
+
+:select t
+
+Make sure the window that popped up is selected afterwards.
+
+:reuse t
+
+Don't pop up any window and reuse the currently active one.
+
+To make an exception to a default rule, use the condition you
+want to exclude and either not use the key in question, use a
+different value or use a placeholder as key."
+  :type '(alist :key-type (choice symbol string)
+                :value-type (plist :options
+                                   ((:regexp boolean)
+                                    (:select boolean))))
+  :group 'shackle)
+
+(setq shackle-rules
+      '(("*svg 2048*" :reuse t)
+        (compilation-mode :noselect t)
+        (t :select t)))
+
+(defun shackle--match (buffer-or-name condition plist)
+  (let* ((buffer (get-buffer buffer-or-name))
+         (buffer-major-mode (buffer-local-value 'major-mode buffer))
+         (buffer-name (buffer-name buffer)))
+    (when (or (eq t condition) ; default case
+              (and (symbolp condition)
+                   (eq condition buffer-major-mode))
+              (and (stringp condition)
+                   (or (string= condition buffer-name)
+                       (and (plist-get plist :regexp)
+                            (string-match condition buffer-name)))))
+      plist)))
+
+(defun shackle-match (buffer-or-name)
+  (cl-loop for (condition . plist) in shackle-rules
+           when (shackle--match buffer-or-name condition plist)
+           return plist))
+
+(defun shackle-display-buffer-condition (buffer action)
+  (message "Buffer: %s, Action: %s" buffer action)
+  (shackle-match buffer))
+
+(defun shackle-display-buffer-action (buffer alist)
+  (message "Buffer: %s, Alist: %s" buffer alist)
+  (shackle-display-buffer buffer alist (shackle-match buffer)))
+
+(defun shackle--splittable-frame ()
+  "Returns a splittable frame to work on.
+This can be either the selected frame or the last frame that's
+not displaying a lone minibuffer."
+  (let ((selected-frame (selected-frame))
+        (last-non-minibuffer-frame (last-nonminibuffer-frame)))
+    (or (and (window--frame-usable-p selected-frame)
+             (not (frame-parameter selected-frame 'unsplittable)))
+        (and (window--frame-usable-p last-non-minibuffer-frame)
+             (not (frame-parameter last-non-minibuffer-frame 'unsplittable))))))
+
+(defun shackle--split-some-window (frame alist)
+  "Returns a window if splitting any window was successful.
+This tries using the largest window for splitting, if all windows
+are the same size, the selected one is taken, in case this fails,
+the least recently used window is used for splitting."
+  (or (window--try-to-split-window (get-largest-window frame t) alist)
+      (window--try-to-split-window (get-lru-window frame t) alist)))
+
+(defun shackle-display-buffer (buffer alist &optional plist)
+  "Display BUFFER and adhere to the conditions in PLIST.
+See `display-buffer-pop-up-window' for the basic functionality
+the majority of code was lifted from.  Additionally to BUFFER AND
+ALIST this function takes an optional PLIST argument which allows
+it to do useful things such as selecting the popped up window
+afterwards."
+  (if (plist-get plist :reuse)
+      (prog1 (window--display-buffer buffer (selected-window) 'window alist)
+        ;; the following is done to ensure a reused window doesn't get
+        ;; killed when invoking `quit-window', a command bound per
+        ;; default to "q" in buffers derived from `special-mode'
+        (set-window-parameter (selected-window) 'quit-restore nil))
+    (let ((frame (shackle--splittable-frame))
+      (when frame
+        (let ((window (shackle--split-some-window frame alist)))
+          (prog1 (window--display-buffer
+                  buffer window 'window alist display-buffer-mark-dedicated)
+            (when (plist-get plist :select)
+              (select-window window t))
+            (unless (cdr (assq 'inhibit-switch-frame alist))
+              (window--maybe-raise-frame (window-frame window))))))))))
+
+(define-minor-mode shackle-mode
+  "Toggle `shackle-mode'.
+This global minor mode allows you to easily set up rules for
+popups in Emacs."
+  :lighter shackle-lighter
+  :global t
+  (if shackle-mode
+      (add-to-list 'display-buffer-alist
+                   '(shackle-display-buffer-condition
+                     shackle-display-buffer-action))
+    (setq display-buffer-alist
+          (remove '(shackle-display-buffer-condition
+                    shackle-display-buffer-action)
+                  display-buffer-alist))))
+
+;;; shackle.el ends here
