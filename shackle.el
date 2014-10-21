8cae14704249f9c80a0d8c3e0d75eafe1c682452
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-10-21 20:55:28 +0200

    Bump version

--- shackle.el
+++ shackle.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
 ;; URL: https://github.com/wasamasa/shackle
-;; Version: 0.0.5
+;; Version: 0.0.6
 ;; Keywords: convenience
 
 ;; This file is NOT part of GNU Emacs.
