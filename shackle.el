746044b4e0c8014f63e275cf3de6c172b1db721d
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-06-15 10:05:30 +0200

    Bump version

--- shackle.el
+++ shackle.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
 ;; URL: https://github.com/wasamasa/shackle
-;; Version: 0.6.0
+;; Version: 0.6.1
 ;; Keywords: convenience
 ;; Package-Requires: ((cl-lib "0.5"))
 
